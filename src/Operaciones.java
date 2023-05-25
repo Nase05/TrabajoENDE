010fd92742098c548bb4f19353ee539ecee0b235
Nerea <nerea3579@gmail.com>
2023-05-25 13:40:16 +0200

    Segundo método: calcular si el número es o no primo

--- src/Operaciones.java
+++ src/Operaciones.java
@@ -25,5 +25,23 @@ public class Operaciones {
 	 return producto;
 	 
 	}
-	
+
+	/** 	 	
+	 * @author Nerea
+	 * Método que reciba un número y devuelva si es primo o
+	 * no. Un número primo es aquel solo puede dividirse entre 1 y si mismo
+	 */
+	public static boolean esPrimo(int numero) {
+		  /**El 0, 1 y 4 no son primos*/
+		  if (numero == 0 || numero == 1 || numero == 4) {
+		    return false;
+		  }
+		  for (int x = 2; x < numero / 2; x++) {
+		   /** Si es divisible por cualquiera de estos números, no es primo*/
+		    if (numero % x == 0)
+		      return false;
+		  }
+		  /** @return Si no se pudo dividir por ninguno de los de arriba, sí es primo*/
+		  return true;
+		}
 } 

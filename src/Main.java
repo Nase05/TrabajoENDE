5840662af98865e973ad7396f28b6c5b9b6b4e37
Ivan <ivanpelora2601@gmail.com>
2023-05-25 13:57:28 +0200

    Metodo de calcular el factorial utilizado en el Main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Operaciones operaciones = new Operaciones();
+		//Método de factorial
+		System.out.println(operaciones.factorial(5));
+		
+	}
+}
